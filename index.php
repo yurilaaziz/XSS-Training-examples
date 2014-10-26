b19fcf2191d9da111013211ef6b55d400692ce52
Med Amine Ben Asker <ben.asker.amine@gmail.com>
2014-10-26 02:43:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("common.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Guestbook</title>
+</head>
+<body>
+ <center>
+	<p><?php display_top();?></p>
+     <hr>
+  <p>
+    <a href="livredor.php">Go to the guestbook </a><br>
+    <a href="signin.php">Sign in </a>
+  </p>
+ </center>
+</body>
+</html>
