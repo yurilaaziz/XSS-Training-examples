5cb8bbb3345b1e2989a532c4e9b84398dd5ee907
Med Amine Ben Asker <ben.asker.amine@gmail.com>
2014-10-26 02:49:22 +0100

    Create search.php

---
+++ search.php
@@ -0,0 +1,56 @@
+<?php
+include("common.php");
+$mysql = $arrayName = array('dbname'     => "xsslab",
+                            'dbuser'     => "root",
+                            'dbhost'     => "localhost",
+                            'dbpass' => "",
+                            );
+
+function find_messages($mysql,$nickname){
+
+ mysql_connect($mysql['dbhost'], $mysql['dbuser'], $mysql['dbpass']);
+ mysql_select_db($mysql['dbname']);
+ 
+ $sql = mysql_query("SELECT * FROM comments WHERE nickname = '".$nickname."'");
+ 
+ // just to skip displaying SQL error messages
+ if (! $sql) return;
+
+if (!mysql_num_rows($sql))
+	echo "There is no comment written by : < ". $nickname ." ><br>";
+while ($w = mysql_fetch_array($sql))
+{
+  echo '<p>
+        <b>'. $w['nickname'].' </b> <i>'.date('h:i:s d/m/Y ', $w['date_enrg']).'</i> <br>
+        '. $w['value'].'
+        </p>
+        ';
+ }
+
+mysql_close();
+}
+
+$nickname = (isset($_GET['nickname']) ? $_GET['nickname'] : NULL );
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>uestbook</title>
+</head>
+
+<body>
+  <center>
+    <p><?php display_top();?></p>
+    <hr>
+      <?php find_messages($mysql,$nickname); ?>
+    <hr>
+    <p>
+      <form method="GET">
+        Search for comment : <input type="text" name="nickname" value="nickname.."><br>
+         <input type="submit" value="send">
+      </form>
+     </p>
+ </center>
+</body>
+</html>
